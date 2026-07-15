42c33721945fd37b18568560718d620b62bf5242
lilively <61706008+lilively@users.noreply.github.com>
2026-07-15 15:03:51 +0200

    fixes

--- Organization/replace_pattern.py
+++ Organization/replace_pattern.py
@@ -42,32 +42,32 @@ def rename_files_in_directory(directory, old_pattern, new_pattern):
     print(f'Failed to rename {len(failed)} files.')
     print(f'Renaming completed in directory: {directory}')
 
-if __name__ == "__main__":
-    import argparse
-    parser = argparse.ArgumentParser(description='Rename files in a directory by replacing a pattern.')
-    parser.add_argument('directory', type=str, 
-                        help='Path to the target directory containing files to rename.')
-    parser.add_argument('--old_pattern', type=str, required=True,
-                        help='Pattern in the filename to be replaced.')
-    parser.add_argument('--new_pattern', type=str, required=True,
-                        help='New pattern to replace the old pattern with.')
+# if __name__ == "__main__":
+#     import argparse
+#     parser = argparse.ArgumentParser(description='Rename files in a directory by replacing a pattern.')
+#     parser.add_argument('directory', type=str, 
+#                         help='Path to the target directory containing files to rename.')
+#     parser.add_argument('--old_pattern', type=str, required=True,
+#                         help='Pattern in the filename to be replaced.')
+#     parser.add_argument('--new_pattern', type=str, required=True,
+#                         help='New pattern to replace the old pattern with.')
     
-    args = parser.parse_args()
+#     args = parser.parse_args()
     
-    rename_files_in_directory(
-        directory=args.directory,
-        old_pattern=args.old_pattern,
-        new_pattern=args.new_pattern
-    )
+#     rename_files_in_directory(
+#         directory=args.directory,
+#         old_pattern=args.old_pattern,
+#         new_pattern=args.new_pattern
+#     )
 
-# working_folder = r"C:\Users\lilif\OneDrive\Desktop\Dropbox\Phd\Pipeline testing\Images\MRS image_grid"
-# mode='replace' #options: 'append', 'replace'
-# old_pattern = '-1'
-# new_pattern = '_1'
+working_folder = r"C:\Users\Lili\Dropbox\Phd\Pseudoprogression\Segm_G\MRSgrid_0.8_NoData"
+mode='replace' #options: 'append', 'replace'
+old_pattern = '_grid'
+new_pattern = ''
 # append_pattern = '-LE'
 
-# rename_files_in_directory(directory=working_folder,
-#                            old_pattern=old_pattern,
-#                              new_pattern=new_pattern,
-#                              mode='replace')
+rename_files_in_directory(directory=working_folder,
+                           old_pattern=old_pattern,
+                             new_pattern=new_pattern,
+                             mode='replace')
 
